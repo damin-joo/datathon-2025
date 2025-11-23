c11e48a3464362c8c4e9807fe3e78a8ede8715f7
dayoungjoo <jooday@sheridancollege.ca>
2025-11-23 11:26:57 -0500

    Changes to be committed: 	new file:   backend/data/1110022301-eng.csv 	modified:   backend/python-functions/decarbon_analysis_model.py

--- backend/python-functions/decarbon_analysis_model.py
+++ backend/python-functions/decarbon_analysis_model.py
@@ -9,3 +9,6 @@ messages['length'] = messages['message'].apply(len)
 
 messages['length'] = messages['message'].apply(len)
 messages.groupby('label').describe()
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3,random_state=87)
